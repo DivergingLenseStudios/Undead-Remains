82328a8e5e1f6d0f176150683b41258de6c0a2fb
jorphil <diverginglensestudios@gmail.com>
2026-05-04 10:50:34 +0200

    addes todos

--- src/main/java/net/diverginglensestudios/undeadremains/Todo.java
+++ src/main/java/net/diverginglensestudios/undeadremains/Todo.java
@@ -1,2 +1,23 @@
-// TODO: Add Paintings
-// TODO: Balance fossil polisher
+/*
+Goals:
+	-Xanas
+		-Add dungeon
+			-Design rooms
+			-Add paintings
+			-Add boss Fossil and Xanarian
+		-Fully add xanarian cannibal
+		-improve worldgen
+			-fix hole in roof
+			-add more block variety
+	-Balancing
+		-Fossil polisher
+		-maybe armor
+	-reworks
+		-Spike shoots spikes upon death
+		-New model for ice zombie
+		-Add weapon skins
+			-Smaller version for harvester
+			-2d version for spike bat
+	-fixes
+		fix rotation of polisher
+ */
