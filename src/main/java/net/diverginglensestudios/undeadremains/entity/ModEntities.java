31a45b513c43733b63131158b638811b131fa107
Jorphil <diverginglensestudios@gmail.com>
2026-05-17 17:48:39 +0200

    fixed hitbox sizes

--- src/main/java/net/diverginglensestudios/undeadremains/entity/ModEntities.java
+++ src/main/java/net/diverginglensestudios/undeadremains/entity/ModEntities.java
@@ -32,72 +32,72 @@ public class ModEntities {
 	public static final RegistryObject<EntityType<UndeadArcheologistEntity>> UNDEAD_ARCHEOLOGIST = ENTITY_TYPES
 			.register("undead_archeologist",
 					() -> EntityType.Builder.of(UndeadArcheologistEntity::new, MobCategory.MONSTER)
-							.sized(0.6f/* width */, 2f/* height */).build("undead_archeologist"));
+							.sized(0.6f/* width */, 1.95f/* height */).build("undead_archeologist"));
 
 	public static final RegistryObject<EntityType<SupremeKnightEntity>> SUPREME_KNIGHT = ENTITY_TYPES
 			.register("supreme_knight", () -> EntityType.Builder.of(SupremeKnightEntity::new, MobCategory.MONSTER)
-					.sized(2f, 4f).build("supreme_knight"));
+					.sized(2f, 3.95f).build("supreme_knight"));
 
 	public static final RegistryObject<EntityType<SahnUzalEntity>> SAHN_UZAL = ENTITY_TYPES.register("sahn_uzal",
 			() -> EntityType.Builder.of(SahnUzalEntity::new, MobCategory.MONSTER)
-					.sized(2f, 4f).build("sahn_uzal"));
+					.sized(2f, 3.95f).build("sahn_uzal"));
 
 	public static final RegistryObject<EntityType<FossilizedZombieEntity>> FOSSILIZED_ZOMBIE = ENTITY_TYPES
 			.register("fossilized_zombie",
 					() -> EntityType.Builder
 							.<FossilizedZombieEntity>of(FossilizedZombieEntity::new, MobCategory.MONSTER)
-							.sized(0.6f, 2f).build("fossilized_zombie"));
+							.sized(0.6f, 1.95f).build("fossilized_zombie"));
 
 	public static final RegistryObject<EntityType<CowZombieEntity>> COW_ZOMBIE = ENTITY_TYPES.register("cow_zombie",
 			() -> EntityType.Builder.<CowZombieEntity>of(CowZombieEntity::new, MobCategory.MONSTER)
-					.sized(1.5f, 2f).build("cow_zombie"));
+					.sized(1.5f, 1.95f).build("cow_zombie"));
 
 	public static final RegistryObject<EntityType<SpikeEntity>> SPIKE = ENTITY_TYPES.register("spike",
 			() -> EntityType.Builder.<SpikeEntity>of(SpikeEntity::new, MobCategory.MONSTER)
-					.sized(0.6f, 2f).build("spike"));
+					.sized(0.6f, 1.95f).build("spike"));
 
 	public static final RegistryObject<EntityType<StrayZombieEntity>> STRAY_ZOMBIE = ENTITY_TYPES.register(
 			"stray_zombie", () -> EntityType.Builder.<StrayZombieEntity>of(StrayZombieEntity::new, MobCategory.MONSTER)
-					.sized(0.6f, 2f).build("stray_zombie"));
+					.sized(0.6f, 1.95f).build("stray_zombie"));
 
 	public static final RegistryObject<EntityType<TreeZombieEntity>> TREE_ZOMBIE = ENTITY_TYPES.register("tree_zombie",
 			() -> EntityType.Builder.<TreeZombieEntity>of(TreeZombieEntity::new, MobCategory.MONSTER)
-					.sized(0.6f, 2.5f).build("tree_zombie"));
+					.sized(0.6f, 2.45f).build("tree_zombie"));
 
 	public static final RegistryObject<EntityType<BigWoodlingEntity>> BIG_WOODLING = ENTITY_TYPES.register(
 			"big_woodling", () -> EntityType.Builder.<BigWoodlingEntity>of(BigWoodlingEntity::new, MobCategory.MONSTER)
-					.sized(0.6f, 1f).build("big_woodling"));
+					.sized(0.6f, 0.95f).build("big_woodling"));
 
 	public static final RegistryObject<EntityType<SmallWoodlingEntity>> SMALL_WOODLING = ENTITY_TYPES.register(
 			"small_woodling",
 			() -> EntityType.Builder.<SmallWoodlingEntity>of(SmallWoodlingEntity::new, MobCategory.MONSTER)
-					.sized(0.6f, 1f).build("small_woodling"));
+					.sized(0.6f, 0.95f).build("small_woodling"));
 
 	public static final RegistryObject<EntityType<HammerZombieEntity>> HAMMER_ZOMBIE = ENTITY_TYPES.register(
 			"hammer_zombie",
 			() -> EntityType.Builder.<HammerZombieEntity>of(HammerZombieEntity::new, MobCategory.MONSTER)
-					.sized(0.6f, 2.25f).build("hammer_zombie"));
+					.sized(0.6f, 2.2f).build("hammer_zombie"));
 
 	public static final RegistryObject<EntityType<FourEyedXanarianEntity>> FOUR_EYED_XANARIAN = ENTITY_TYPES
 			.register("four_eyed_xanarian",
 					() -> EntityType.Builder
 							.<FourEyedXanarianEntity>of(FourEyedXanarianEntity::new, MobCategory.MONSTER)
-							.sized(1.4f, 2.5f).build("four_eyed_xanarian"));
+							.sized(1.4f, 2.45f).build("four_eyed_xanarian"));
 
 	public static final RegistryObject<EntityType<XanarianEntity>> XANARIAN = ENTITY_TYPES.register("xanarian",
 			() -> EntityType.Builder.<XanarianEntity>of(XanarianEntity::new, MobCategory.MONSTER)
-					.sized(1.4f, 4f).build("xanarian"));
+					.sized(1.4f, 3.95f).build("xanarian"));
 
 	public static final RegistryObject<EntityType<HornedXanarianEntity>> HORNED_XANARIAN = ENTITY_TYPES.register(
 			"horned_xanarian",
 			() -> EntityType.Builder.<HornedXanarianEntity>of(HornedXanarianEntity::new, MobCategory.MONSTER)
-					.sized(1.5f, 4.5f).build("horned_xanarian"));
+					.sized(1.5f, 4.45f).build("horned_xanarian"));
 
 	public static final RegistryObject<EntityType<XanarianCannibalEntity>> XANARIAN_CANNIBAL = ENTITY_TYPES
 			.register("xanarian_cannibal",
 					() -> EntityType.Builder
 							.<XanarianCannibalEntity>of(XanarianCannibalEntity::new, MobCategory.MONSTER)
-							.sized(1.4f, 3.25f).build("xanarian_cannibal"));
+							.sized(1.4f, 3.2f).build("xanarian_cannibal"));
 
 	public static final RegistryObject<EntityType<XanaranhaEntity>> XANARANHA = ENTITY_TYPES.register("xanaranha",
 			() -> EntityType.Builder.<XanaranhaEntity>of(XanaranhaEntity::new, MobCategory.WATER_CREATURE)
@@ -105,15 +105,15 @@ public class ModEntities {
 
 	public static final RegistryObject<EntityType<SuckerEntity>> SUCKER = ENTITY_TYPES.register("sucker",
 			() -> EntityType.Builder.<SuckerEntity>of(SuckerEntity::new, MobCategory.WATER_CREATURE)
-					.sized(1f, 1f).build("sucker"));
+					.sized(1f, 0.95f).build("sucker"));
 
 	public static final RegistryObject<EntityType<SupremeZombieEntity>> SUPREME_ZOMBIE = ENTITY_TYPES
 			.register("supreme_zombie", () -> EntityType.Builder.of(SupremeZombieEntity::new, MobCategory.MONSTER)
-					.sized(0.6f, 2f).build("supreme_zombie"));
+					.sized(0.6f, 1.95f).build("supreme_zombie"));
 
 	public static final RegistryObject<EntityType<DrownedSkeletonEntity>> DROWNED_SKELETON = ENTITY_TYPES
 			.register("drowned_skeleton", () -> EntityType.Builder.of(DrownedSkeletonEntity::new, MobCategory.MONSTER)
-					.sized(0.6f, 2f).build("drowned_skeleton"));
+					.sized(0.6f, 1.95f).build("drowned_skeleton"));
 
 	public static final RegistryObject<EntityType<ModBoatEntity>> MOD_BOAT = ENTITY_TYPES.register("mod_boat",
 			() -> EntityType.Builder.<ModBoatEntity>of(ModBoatEntity::new, MobCategory.MISC)
