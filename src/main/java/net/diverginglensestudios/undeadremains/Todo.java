b6b2e0654fdf3aebc945ebe12bb0006e7eef89eb
Lom Jek <lomjekp@gmail.com>
2026-07-13 17:44:15 +0200

    Moved Bugs to ISSUES

--- src/main/java/net/diverginglensestudios/undeadremains/Todo.java
+++ src/main/java/net/diverginglensestudios/undeadremains/Todo.java
@@ -15,10 +15,4 @@ Goals:
 	-reworks
 		-Spike shoots spikes upon death
 		-Add weapon skins
-	-fixes
-		fix flowers showing as 3D in hand
-		fix [17:04:46] [Worker-Main-2/ERROR] [minecraft/Util]: Detected setBlock in a far chunk [23, -40], pos: MutableBlockPos{x=370, y=124, z=-640},
-		status: minecraft:features, currently generating: ResourceKey[minecraft:worldgen/placed_feature / undeadremains:xanas_flower_placed]
-		fix trapdoor texture
-		fix block.tutorialmod.gem_polishing_station in JEI (/src/main/java/net/diverginglensestudios/undeadremains/recipe/GemPolishingRecipe.java)
  */
